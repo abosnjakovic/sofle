8e25d58105cdc000f40734e00d13c9dfcbef51c5
Adam Bosnjakovic <adam@adimension.io>
2021-04-29 15:02:53 +1000

    remove other keyboards

--- keyboards/sofle/rev1/config.h
+++ keyboards/sofle/rev1/config.h
@@ -1,11 +1,11 @@
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFC32
-#define PRODUCT_ID      0x0287
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    JosefAdamcik
-#define PRODUCT         Sofle
+#define VENDOR_ID 0xFC32
+#define PRODUCT_ID 0x0287
+#define DEVICE_VER 0x0001
+#define MANUFACTURER JosefAdamcik
+#define PRODUCT Sofle
 
 /* key matrix size */
 // Rows are doubled-up
@@ -13,18 +13,24 @@
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS \
+    { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS \
+    { F6, F7, B1, B3, B2, B6 }
 #define DIODE_DIRECTION COL2ROW
 
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #define DEBOUNCE 5
 
 /* encoder support */
-#define ENCODERS_PAD_A { F5 }
-#define ENCODERS_PAD_B { F4 }
-#define ENCODERS_PAD_A_RIGHT { F4 }
-#define ENCODERS_PAD_B_RIGHT { F5 }
+#define ENCODERS_PAD_A \
+    { F5 }
+#define ENCODERS_PAD_B \
+    { F4 }
+#define ENCODERS_PAD_A_RIGHT \
+    { F4 }
+#define ENCODERS_PAD_B_RIGHT \
+    { F5 }
 #define ENCODER_RESOLUTION 2
 
 #define TAP_CODE_DELAY 10
@@ -36,4 +42,3 @@
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
